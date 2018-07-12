aee5d318a5cd9fe7c6f0c7c3ecc4b7a69408a951
Charles McQuain <charlesmcquain@gmail.com>
2018-07-11 21:48:15 -0700

    Imported Navbar component to Main App

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from "./Navbar";
+
 export default {
   name: "app",
+  components: {
+    Navbar
+  },
   data() {
     return {
       title: "Welcome to Your Vue.js App"
