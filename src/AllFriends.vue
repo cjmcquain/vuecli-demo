a29165850ae70285b25db0dca4eb3296e5f7e231
Charles McQuain <charlesmcquain@gmail.com>
2018-07-12 23:09:52 -0700

    Created AllFriends component

---
+++ src/AllFriends.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="all-friends">
+    <h2>All Friends</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AllFriends",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
