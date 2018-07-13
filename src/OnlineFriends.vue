df6118880ad99ceb92a5e2d7f8876781362916d1
Charles McQuain <charlesmcquain@gmail.com>
2018-07-12 23:10:25 -0700

    Created OnlineFriends component

---
+++ src/OnlineFriends.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="online-friends">
+    <h2>Online Friends</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OnlineFriends",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
