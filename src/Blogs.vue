5e47f9f8d2c6860727171b306284516998fb6686
Charles McQuain <charlesmcquain@gmail.com>
2018-07-19 22:15:29 -0700

    Installed Axios and brought in JSON data from JSON placeholder

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="blogs">
     <h2>{{ blogTitle }}</h2>
-    <button @click="changeTitle">Change Title</button>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Blogs",
   data() {
     return {
-      blogTitle: "Blogs"
+      blogTitle: "Blog site",
+      posts: []
     };
   },
   methods: {
@@ -22,7 +28,15 @@ export default {
     alert("beforeCreate hook");
   },
   created() {
-    alert("created hook");
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/")
+      .then(response => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   beforeUpdate() {
     alert("beforeUpdate hook");
