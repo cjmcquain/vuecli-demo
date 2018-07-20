eae2b3e3b3cb2aca0731a6df2798593231916428
Charles McQuain <charlesmcquain@gmail.com>
2018-07-19 21:19:42 -0700

    Created Blogs component

---
+++ src/Blogs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="blogs">
+    <h2>{{ blogTitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      blogTitle: "Blogs"
+    };
+  }
+};
+</script>
