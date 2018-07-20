0c79487600b6720735f6667e2edae8a3a97e7e4d
Charles McQuain <charlesmcquain@gmail.com>
2018-07-19 22:03:31 -0700

    Demonstrated lifecycle hooks beforeCreate, created, and beforeUpdate

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="blogs">
     <h2>{{ blogTitle }}</h2>
+    <button @click="changeTitle">Change Title</button>
   </div>
 </template>
 
@@ -11,6 +12,20 @@ export default {
     return {
       blogTitle: "Blogs"
     };
+  },
+  methods: {
+    changeTitle() {
+      this.blogTitle = "Amazing Blog Site";
+    }
+  },
+  beforeCreate() {
+    alert("beforeCreate hook");
+  },
+  created() {
+    alert("created hook");
+  },
+  beforeUpdate() {
+    alert("beforeUpdate hook");
   }
 };
 </script>
