b9fa3b0b927bdce0427a4501b340b9b3139cf88b
Charles McQuain <charlesmcquain@gmail.com>
2018-07-20 22:33:43 -0700

    Added search bar to filter through blog posts

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -2,7 +2,7 @@
   <div class="blogs">
     <h2>{{ blogTitle }}</h2>
     <input type="text" v-model="searchTerm">
-    <div v-for="post in posts" :key="post.id">
+    <div v-for="post in filteredPosts" :key="post.id">
       <h3>{{ post.title }}</h3>
       <p>{{ post.body | snippet }}</p>
     </div>
@@ -33,9 +33,6 @@ export default {
       });
     }
   },
-  beforeCreate() {
-    alert("beforeCreate hook");
-  },
   created() {
     axios
       .get("https://jsonplaceholder.typicode.com/posts/")
@@ -46,9 +43,6 @@ export default {
       .catch(err => {
         console.log(err);
       });
-  },
-  beforeUpdate() {
-    alert("beforeUpdate hook");
   }
 };
 </script>
